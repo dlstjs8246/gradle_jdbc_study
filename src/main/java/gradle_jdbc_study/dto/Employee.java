39d956f2d8962ce73f6053bc96c5a233154f2694
Sunny Park <dlstjs8246@naver.com>
2020-02-10 14:44:06 +0900

    Employee dto 추가

---
+++ src/main/java/gradle_jdbc_study/dto/Employee.java
@@ -0,0 +1,120 @@
+package gradle_jdbc_study.dto;
+
+import java.util.Date;
+
+public class Employee {
+	private int empNo;
+	private String empName;
+	private Title title;
+	private Employee manager;
+	private int salary;
+	private Department dept;
+	private Date hireDate;
+	private byte[] pic;
+	public Employee() {
+
+	}
+	public Employee(int empNo) {
+		super();
+		this.empNo = empNo;
+	}
+	public Employee(int empNo, String empName, Title title, Employee manager, int salary, Department dept,
+			Date hireDate) {
+		super();
+		this.empNo = empNo;
+		this.empName = empName;
+		this.title = title;
+		this.manager = manager;
+		this.salary = salary;
+		this.dept = dept;
+		this.hireDate = hireDate;
+	}
+	public Employee(int empNo, String empName, Title title, Employee manager, int salary, Department dept,
+			Date hireDate, byte[] pic) {
+		super();
+		this.empNo = empNo;
+		this.empName = empName;
+		this.title = title;
+		this.manager = manager;
+		this.salary = salary;
+		this.dept = dept;
+		this.hireDate = hireDate;
+		this.pic = pic;
+	}
+	public int getEmpNo() {
+		return empNo;
+	}
+	public void setEmpNo(int empNo) {
+		this.empNo = empNo;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	public Title getTitle() {
+		return title;
+	}
+	public void setTitle(Title title) {
+		this.title = title;
+	}
+	public Employee getManager() {
+		return manager;
+	}
+	public void setManager(Employee manager) {
+		this.manager = manager;
+	}
+	public int getSalary() {
+		return salary;
+	}
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+	public Department getDept() {
+		return dept;
+	}
+	public void setDept(Department dept) {
+		this.dept = dept;
+	}
+	public Date getHireDate() {
+		return hireDate;
+	}
+	public void setHireDate(Date hireDate) {
+		this.hireDate = hireDate;
+	}
+	public byte[] getPic() {
+		return pic;
+	}
+	public void setPic(byte[] pic) {
+		this.pic = pic;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + empNo;
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Employee other = (Employee) obj;
+		if (empNo != other.empNo)
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return String.format(
+				"Employee [%s, %s, %s, %s, %s, %s, %s, %s]", empNo,
+				empName, title.getTitleNo(), manager.getEmpNo(), salary, dept.getDeptNo(), String.format("%1$tF %1tT$",hireDate),pic==null?null:pic.length);
+	}
+	
+}
+
